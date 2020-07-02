04c0fb6527e65100668c859883d4b2de34f7f90c
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-07-02 23:54:07 +0900

    add user

---
+++ src/User.ts
@@ -0,0 +1,19 @@
+import faker =  require('faker');
+
+class User {
+    name: string;
+    location: {
+        latitude: number;
+        longitude: number;
+    };
+
+    constructor() {
+        this.name = faker.name.firstName();
+        this.location = {
+            latitude: parseFloat(faker.address.latitude()),
+            longitude: parseFloat(faker.address.longitude())
+        }
+    }
+}
+
+export default User;
